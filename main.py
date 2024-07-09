7b52b68070f2c74f00a6fd4b1488045babb7e1cb
jorge <patrullero.matt@gmail.com>
2024-07-10 01:28:30 +0200

    v0.04 Minor improvements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-__version__ = "0.03"
+__version__ = "0.04"
 
 import asyncio
 import os
@@ -8,8 +8,20 @@ import httpx
 import telegram
 from dotenv import load_dotenv
 
-
 load_dotenv("vars.env")
+
+
+async def get_bot(token: str):
+    bot = telegram.Bot(token)
+    async with bot:
+        return await bot.get_me()
+
+
+async def get_info(token: str):
+    bot = telegram.Bot(token)
+    async with bot:
+        updates = (await bot.get_updates())[0]
+        return updates
 
 
 async def send_telegram_message(token: str, chat_id: str, message: str):
@@ -18,22 +30,23 @@ async def send_telegram_message(token: str, chat_id: str, message: str):
         await bot.send_message(text=message, chat_id=chat_id)
 
 
-url = "https://baumguitars.com/2023/10/28/waiting-for-a-baum-check-here/"
-
-regex = r'(?<=<div class=\"circle-val\"><span class=\"circle-counter__number\" data-to-value=\")(.*?)(?=\" data-delimiter=\",\">0<\/span><span class=\"circle-counter__suffix\">%)'
-
-if __name__ == "__main__":
+def get_text_to_send(url: str) -> str:
     content = httpx.get(url)
     test_str = content.content.decode("utf-8")
+    regex = r'(?<=<div class=\"circle-val\"><span class=\"circle-counter__number\" data-to-value=\")(.*?)(?=\" data-delimiter=\",\">0<\/span><span class=\"circle-counter__suffix\">%)'
     matches = re.findall(regex, test_str, re.MULTILINE)
-
     nidhogg_status = matches[8:12]
     nidhogg_status = [f"{i}%" if int(i) < 100 else "👍" for i in nidhogg_status]
-
     production, shipping_dk, qc_setup, shipping_me = nidhogg_status
+    return f"""🎸 NIDHOGG STATUS 🎸\n"""\
+           f"""Production: {production}\n"""\
+           f"""Shipping to DK: {shipping_dk}\n"""\
+           f"""QC and Setup: {qc_setup}\n"""\
+           f"""Shipping to You: {shipping_me}"""
 
-    text_to_send = f"""🎸 NIDHOGG STATUS 🎸\nProduction: {production}\nShipping to DK: {shipping_dk}\nQC and Setup: {qc_setup}\nShipping to You: {shipping_me} """
 
+if __name__ == "__main__":
+    text_to_send = get_text_to_send(url=os.environ.get("URL"))
     asyncio.run(
         send_telegram_message(
             token=os.environ.get("TOKEN"),
